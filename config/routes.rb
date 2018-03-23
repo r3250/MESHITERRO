f22b98829f6292abbdf33555ed88c3c822ae7ffc
r3250 <n0325.0329@gmail.com>
2018-03-23 12:00:20 +0000

    loofahupdate

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+	
   devise_for :users
-
-  root 'post_images#index'
-
   resources :users, only:[:show, :edit, :update]
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
-  		resource :favorites, only:[:create, :destroy]
-  		resource :post_comments,only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
+    resource :post_comments,only:[:create, :destroy]
   end
+
+
+  root 'post_images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
